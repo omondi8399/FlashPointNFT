5b070c9c96b9d03e9cd92da18c7eb1e71f962585
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-02 15:51:03 +0300

    added Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-hero">
         <Header />
+        <Hero />
       </div>
     </div>
   )
