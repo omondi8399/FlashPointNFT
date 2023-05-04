1d9102d610c8b182118aecd8327aa3f8c1b7f80e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-04 10:28:56 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Artworks from "./components/Artworks"
+import CreateNFT from "./components/CreateNFT"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
@@ -14,6 +15,7 @@ const App = () => {
         <Artworks />
         <Transactions />
         <Footer />
+        <CreateNFT />
     </div>
   )
 }
