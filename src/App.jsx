7c229838fa15884ea88102c9fd38b7cc46b471a5
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-06 10:01:26 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import ShowNFT from "./components/ShowNFT"
 import Transactions from "./components/Transactions"
+import UpdateNFT from "./components/UpdateNFT"
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Footer />
         <CreateNFT />
         <ShowNFT />
+        <UpdateNFT />
     </div>
   )
 }
