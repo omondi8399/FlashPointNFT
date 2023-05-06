59929bef52c54dd1b198767a5963f89d32146234
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-06 10:03:43 +0300

    Updated Store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,7 +2,8 @@ import { createGlobalState } from "react-hooks-global-state";
 
 const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
     modal: "scale-0",
-    showModal: "scale-100"
+    showModal: "scale-0",
+    updateModal: "scale-0"
 })
 
 export { useGlobalState, setGlobalState, getGlobalState }
