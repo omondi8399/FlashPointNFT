bcc3fd21efbd5f5b1362c803318c31dfaa3865c3
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-06 10:41:36 +0300

    Updated Store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,7 +3,13 @@ import { createGlobalState } from "react-hooks-global-state";
 const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
     modal: "scale-0",
     showModal: "scale-0",
-    updateModal: "scale-0"
+    updateModal: "scale-0",
+    loading: { show: false, msg: ''}
 })
 
-export { useGlobalState, setGlobalState, getGlobalState }
+const setLoadingMsg = (msg) => {
+    const loading = getGlobalState('loading')
+    setGlobalState('loading', {...loading, msg })
+}
+
+export { useGlobalState, setGlobalState, getGlobalState, setLoadingMsg }
