acf3eb550f45d7542e9f3201eb52d71372d76902
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-07 10:57:02 +0300

    updated Heeader Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+import { connectWallet } from "../Blockchain.services"
 import timelessLogo from "../assets/timeless.png"
+import { useGlobalState, truncate } from '../store'
 
 const Header = () => {
+  const [connectedAccount] = useGlobalState('connectedAccount')
+
   return (
     <div className="w-4/5 flex justify-between md:justify-center items-center py-4 mx-auto">
         <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -14,10 +18,24 @@ const Header = () => {
           <li className="mx-4 cursor-pointer">Community</li>
         </ul>
 
-        <button className="shadow-xl shadow-black text-white bg-[#e32970] hover:bg-[#bd255f] md:text-xs
-        p-2 rounded-full">
+        {connectedAccount ? (
+        <button
+          className="shadow-xl shadow-black text-white
+        bg-[#e32970] hover:bg-[#bd255f] md:text-xs p-2
+          rounded-full cursor-pointer"
+        >
+          {truncate(connectedAccount, 4, 4, 11)}
+        </button>
+      ) : (
+        <button
+          className="shadow-xl shadow-black text-white
+        bg-[#e32970] hover:bg-[#bd255f] md:text-xs p-2
+          rounded-full cursor-pointer"
+          onClick={connectWallet}
+        >
           Connect Wallet
         </button>
+      )}
     </div>
   )
 }
