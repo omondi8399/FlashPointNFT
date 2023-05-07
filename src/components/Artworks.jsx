16f44743e95522ab69706b1253e3a27743fadcf4
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-07 15:34:36 +0300

    updated Artworks.jsx

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -1,45 +1,81 @@
-import { setGlobalState } from "../store"
-
-const imgHero= "https://images.cointelegraph.com/images/1434_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS91cGxvYWRzLzIwMjEtMDYvNGE4NmNmOWQtODM2Mi00YmVhLThiMzctZDEyODAxNjUxZTE1LmpwZWc=.jpg"
+import { useEffect, useState } from 'react'
+import { setGlobalState, useGlobalState } from '../store'
 
 const Artworks = () => {
+  const [nfts] = useGlobalState('nfts')
+  const [end, setEnd] = useState(4)
+  const [count] = useState(4)
+  const [collection, setCollection] = useState([])
+
+  const getCollection = () => {
+    return nfts.slice(0, end)
+  }
+
+  useEffect(() => {
+    setCollection(getCollection())
+  }, [nfts, end])
+
   return (
-    <div className='bg-[#151c25] gradient-bg-artworks'>
-        <div className='w-4/5 py-10 mx-auto'>
-            <h4 className='text-white text-3xl font-bold uppercase text-gradient'>Latest Artworks</h4>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4
-            gap-6 md:gap-4 lg:gap-3 py-2.5'>
-                {Array(4).fill().map((nft, i) => (
-                    <Card key={i} nft={i + 1} />
-                ))}
-            </div>
-            
-            <div className="text-center my-5">
-            <button className="shadow-lg shadow-black text-sm 
-            bg-[#e32970] hover:bg-[#bd255f] p-2 rounded-full">Load More </button>
-            </div>
+    <div className="bg-[#151c25] gradient-bg-artworks">
+      <div className="w-4/5 py-10 mx-auto">
+        <h4 className="text-white text-3xl font-bold uppercase text-gradient">
+          {collection.length > 0 ? 'Latest Artworks' : 'No Artworks Yet'}
+        </h4>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-4 lg:gap-3 py-2.5">
+          {collection.map((nft, i) => (
+            <Card key={i} nft={nft} />
+          ))}
         </div>
+
+        {collection.length > 0 && nfts.length > collection.length ? (
+          <div className="text-center my-5">
+            <button
+              className="shadow-xl shadow-black text-white
+            bg-[#e32970] hover:bg-[#bd255f]
+            rounded-full cursor-pointer p-2"
+              onClick={() => setEnd(end + count)}
+            >
+              Load More
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
 
-const Card = ({ nft }) => (
+const Card = ({ nft }) => {
+  const setNFT = () => {
+    setGlobalState('nft', nft)
+    setGlobalState('showModal', 'scale-100')
+  }
+
+  return (
     <div className="w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3">
-        <img className="h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3" src={imgHero} alt={"NFT Image"} />
-        <h4 className="text-white font-semibold">NFT #{nft}</h4>
-        <p className="text-gray-400 text-sm my-1">This is a long paragraph section</p>
-
-        <div className="flex justify-between items-center mt-3 text-white">
-            <div className="flex flex-col">
-                <small className="text-xs">Current Price</small>
-                <p className="text-sm font-semibold">0.34ETH</p>
-            </div>
-
-            <button className="shadow-lg shadow-black text-sm 
-            bg-[#e32970] hover:bg-[#bd255f] px-1.5 py-1 rounded-full"
-            onClick={() => setGlobalState('showModal', 'scale-100')}>View Details </button>
+      <img
+        src={nft.metadataURI}
+        alt={nft.title}
+        className="h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3"
+      />
+      <h4 className="text-white font-semibold">{nft.title}</h4>
+      <p className="text-gray-400 text-xs my-1">{nft.description}</p>
+      <div className="flex justify-between items-center mt-3 text-white">
+        <div className="flex flex-col">
+          <small className="text-xs">Current Price</small>
+          <p className="text-sm font-semibold">{nft.cost} ETH</p>
         </div>
+
+        <button
+          className="shadow-lg shadow-black text-white text-sm bg-[#e32970]
+            hover:bg-[#bd255f] cursor-pointer rounded-full px-1.5 py-1"
+          onClick={setNFT}
+        >
+          View Details
+        </button>
+      </div>
     </div>
-)
+  )
+}
 
 export default Artworks
