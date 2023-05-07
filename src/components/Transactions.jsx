9d1bf638ad7b6e7eb03d85bea95412c2f9f7914e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-07 15:39:31 +0300

    updated Transactions.jsx

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,48 +1,72 @@
-import { BiTransfer } from "react-icons/bi"
-import { MdOpenInNew } from "react-icons/md"
+import { useEffect, useState } from 'react'
+import { BiTransfer } from 'react-icons/bi'
+import { MdOpenInNew } from 'react-icons/md'
+import { useGlobalState, truncate } from '../store'
 
 const Transactions = () => {
+  const [transactions] = useGlobalState('transactions')
+  const [end, setEnd] = useState(3)
+  const [count] = useState(3)
+  const [collection, setCollection] = useState([])
+
+  const getCollection = () => {
+    return transactions.slice(0, end)
+  }
+
+  useEffect(() => {
+    setCollection(getCollection())
+  }, [transactions, end])
+
   return (
     <div className="bg-[#151c25]">
-        <div className="w-4/5 py-10 mx-auto">
-            <h4 className="text-white text-3xl font-bold
-            uppercase text-gradient">Latest Transactions</h4>
+      <div className="w-4/5 py-10 mx-auto">
+        <h4 className="text-white text-3xl font-bold uppercase text-gradient">
+          {collection.length > 0 ? 'Latest Transactions' : 'No Transaction Yet'}
+        </h4>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
-            gap-6 md:gap-4 lg:gap-2 py-2.5'>
-                {Array(3).fill().map((nft, i) => (
-                    <Transaction key={i} tx={i + 1} />
-                ))}
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-4 lg:gap-2 py-2.5">
+          {collection.map((tx) => (
+            <div
+              key={tx.id}
+              className="flex justify-between items-center border border-pink-500 text-gray-400 w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3"
+            >
+              <div className="rounded-md shadow-sm shadow-pink-500 p-2">
+                <BiTransfer />
+              </div>
 
-            <div className="text-center my-5">
-            <button className="shadow-lg shadow-black text-sm 
-            bg-[#e32970] hover:bg-[#bd255f] p-2 rounded-full">Load More </button>
+              <div>
+                <h4 className="text-sm">{tx.title} Transfered</h4>
+                <small className="flex flex-row justify-start items-center">
+                  <span className="mr-1">Received by</span>
+                  <a href="#" className="text-pink-500 mr-2">
+                    {truncate(tx.owner, 4, 4, 11)}
+                  </a>
+                  <a href="#">
+                    <MdOpenInNew />
+                  </a>
+                </small>
+              </div>
+
+              <p className="text-sm font-medium">{tx.cost}ETH</p>
             </div>
+          ))}
         </div>
+
+        {collection.length > 0 && transactions.length > collection.length ? (
+          <div className="text-center my-5">
+            <button
+              className="shadow-xl shadow-black text-white
+            bg-[#e32970] hover:bg-[#bd255f]
+            rounded-full cursor-pointer p-2"
+              onClick={() => setEnd(end + count)}
+            >
+              Load More
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
-
-const Transaction = ({ tx }) => (
-    <div className="flex justify-between items-center border border-pink-500 
-    text-gray-400 w-full shadow-xl shadow-black rounded-md
-    overflow-hidden bg-gray-800 my-2 py-3">
-        <div className="rounded-md shadow-sm shadow-pink-500 p-2">
-            <BiTransfer />
-        </div>
-
-        <div>
-            <h4 className="text-sm">{tx} Fund Transfered </h4>
-            <small className="flex justify-start items-center">
-                <span className="mr-1">Received by</span>
-                <a className="text-pink-500 mr-2" href="#" target="_blank">0x31...037</a>
-                <MdOpenInNew />
-            </small>
-        </div>
-
-        <p className="text-sm font-medium">0.32 ETH</p>
-    </div>
-)
 
 export default Transactions
